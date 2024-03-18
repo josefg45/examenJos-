8ec64160eb77102151ccccdbf0793b2be611223b
josefg45 <josefg45@educastur.es>
2024-03-18 13:11:34 +0100

    Añadidos comentarios de documentación en claseExamen

--- examenJose/src/miexamen/Claseexamen.java
+++ examenJose/src/miexamen/Claseexamen.java
@@ -1,5 +1,13 @@
 
 package miexamen;
+/**
+ * @author José Fernández García
+ * <h1>Entornos de desarrollo UT3</h1>
+ * <p>Esta clase sirve para realizar el examen de la UT3 del módulo Entorno de Desarrollo</p>
+ * <p><a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU">Curso de Git y github de pildoras informáticas</p>
+ * @version 1.0.0
+ * @since 18/03/2024
+ */
 
 public class Claseexamen {
 
