f927ccb32750b908e86a8f963e21a51b62dba390
josefg45 <josefg45@educastur.es>
2024-03-18 12:33:21 +0100

    Añadidos los primeros ficheros al proyecto

---
+++ examenJose/src/miexamen/Claseexamen.java
@@ -0,0 +1,12 @@
+
+package miexamen;
+
+public class Claseexamen {
+
+
+    public static void main(String[] args) {
+        System.out.println("Estoy haciendo el examne de la UT3");
+       
+    }
+    
+}
